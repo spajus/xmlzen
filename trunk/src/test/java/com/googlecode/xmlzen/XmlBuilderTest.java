0091739349551f5e1bf769bdecbf994a20c965db
tomas.varaneckas <tomas.varaneckas@52ce8190-9228-11de-8e1b-e3dee1b55d7a>
2009-08-27 17:32:03 +0000

    XmlBuilder documentation.

--- trunk/src/test/java/com/googlecode/xmlzen/XmlBuilderTest.java
+++ trunk/src/test/java/com/googlecode/xmlzen/XmlBuilderTest.java
@@ -25,4 +25,28 @@ public class XmlBuilderTest {
         		"<your xml=\"nicely\"/></xml>", xml);
     }
     
+    @Test
+	public void testComplexBuild() throws Exception {
+        String xml = XmlBuilder.newXml("utf-8")
+  		    .openTag("root").withAttribute("id", 1).withAttribute("one", "two")
+  		        .openTag("inner1")
+  		        .openTag("inner11").withAttribute("x", "y") 
+       			    .withValue("somthing inside")
+       			.closeTag()	
+       			.openTag("empty").closeTag()
+       			.openTag("attr-only").withAttribute("attr", 123)
+       			.closeTag()
+       		.closeTag()
+       		.openTag("inner2")
+       		    .withValue("another boring value")
+       		.closeAllTags()
+       	.toString();
+        assertEquals("<?xml version=\"1.0\" encoding=\"utf-8\"?>" +
+        		"<root id=\"1\" one=\"two\"><inner1>" +
+        		"<inner11 x=\"y\">somthing inside</inner11><empty/>" +
+        		"<attr-only attr=\"123\"/></inner1>" +
+        		"<inner2>another boring value</inner2></root>", xml);
+        log.debug(xml);
+	}
+    
 }
