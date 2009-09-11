1f54b5635407af1a8fa49f80990f8896bdb751c2
tomas.varaneckas <tomas.varaneckas@52ce8190-9228-11de-8e1b-e3dee1b55d7a>
2009-09-11 17:52:15 +0000

    Comments and examples

--- src/main/java/com/googlecode/xmlzen/XmlSlicer.java
+++ src/main/java/com/googlecode/xmlzen/XmlSlicer.java
@@ -16,25 +16,94 @@
  */
 package com.googlecode.xmlzen;
 
+import java.util.List;
+
 import com.googlecode.xmlzen.utils.Value;
 import com.googlecode.xmlzen.utils.XmlUtils;
 
+/**
+ * A Slicer for cutting XML into pieces. Some examples:
+ * 
+ * <p>#1 - get the value of a tag:</p>
+ * <pre>
+ * String xml = "&lt;some&gt;stuff&lt;/some&gt;";
+ * //value of stuff will be 'stuff'
+ * String stuff = XmlSlicer.cut(xml).get("some");
+ * </pre>
+ * 
+ * TODO - add more examples
+ * 
+ * @author Tomas Varaneckas &lt;tomas.varaneckas@gmail.com&gt;
+ * @version $Id$
+ */
 public class XmlSlicer {
 
+    /**
+     * A piece of XML that is currently being sliced
+     */
     private final String xml;
     
-    private XmlSlicer(String xml) {
+    /**
+     * A private constructor that builds the XmlSlicer. Use 
+     * {@link #cut(String)} for instantiating XmlSlicer.
+     * 
+     * @see #cut(String)
+     * @param xml
+     */
+    private XmlSlicer(final String xml) {
         this.xml = xml;
     }
     
+    /**
+     * Factory method that requires an XML in form of a String.
+     * You can make a String from file by using FileUtils.readFile()
+     * 
+     * @param xml
+     */
     public static XmlSlicer cut(final String xmlString) {
         return new XmlSlicer(xmlString);
     }
     
+    /**
+     * Gets the contents of an XML tag. Example:
+     * 
+     * <pre>
+     * //val will be 'xml'
+     * String val = XmlSlicer.cut("&lt;an&gt;xml&lt;/an&gt;").getValue("an");
+     * </pre>
+     * 
+     * Warning. You lose any attributes that <code>tag</code> was holding.
+     * Use {@link #getAttribute(String, String)} before {@link #get(String)} if 
+     * you need them.
+     * 
+     * @param tag target tag name
+     * @return contents that are between <tag> and </tag>
+     */
     public XmlSlicer get(final String tag) {
         return new XmlSlicer(XmlUtils.getTagValue(xml, tag));
     }
 
+    /**
+     * Gets an XmlSlicerList (which is a {@link List}&lt;XmlSlicer&gt) that 
+     * contains values of tags that share the same <code>tag</code> name.
+     * <p>Example use:</p>
+     * 
+     * <pre>
+     * String xml = 
+     * "&lt;birds&gt;" +
+     *   "&lt;bird&gt;pidgeon&lt;/bird&gt;" +
+     *   "&lt;bird&gt;crow&lt;/bird&gt;" +
+     * "&lt;/birds&gt;
+     * //would print: "Bird: pidgeon" and "Bird: crow" 
+     * for (String bird : XmlSlicer.cut(xml).getAll(bird).asList()) {
+     *     System.out.println("Bird: " + bird);
+     * }
+     * </pre>
+     * 
+     * @see XmlSlicerList
+     * @param tag xml tag name
+     * @return XmlSlicerList - a List of XmlSlicer objects
+     */
     public XmlSlicerList getAll(final String tag) {
         final Value<Integer> startOffset = new Value<Integer>(0);
         final Value<Integer> lastOffset = new Value<Integer>(0);
@@ -52,7 +121,20 @@ public class XmlSlicer {
         return results;
     }
     
-    public String getAttribute(String tag, String attribute) {
+    /**
+     * Gets the attribute value from an XML tag. Example:
+     * 
+     * <pre>
+     * //value of attr will be 'val'
+     * String attr = XmlSlicer.cut("&lt;xml attr="val"/&gt;")\
+     *     .getAttribute("xml", "attr");
+     * </pre>
+     * 
+     * @param tag tag name
+     * @param attribute attribute name
+     * @return tag attribute's value 
+     */
+    public String getAttribute(final String tag, final String attribute) {
         return XmlUtils.getAttribute(xml, tag, attribute);
     }
     
