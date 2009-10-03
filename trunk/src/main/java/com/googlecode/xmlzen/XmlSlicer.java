21091232f6f28b2891616449a8bb3a13d54a6ab1
tomas.varaneckas <tomas.varaneckas@52ce8190-9228-11de-8e1b-e3dee1b55d7a>
2009-10-03 17:28:55 +0000

    Updated comments

--- trunk/src/main/java/com/googlecode/xmlzen/XmlSlicer.java
+++ trunk/src/main/java/com/googlecode/xmlzen/XmlSlicer.java
@@ -232,7 +232,7 @@ public class XmlSlicer {
      * <pre>
      * //value of val will be '123'
      * String val = XmlSlicer.cut(&lt;xml&gt;123&lt;/xml&gt;).value();
-     * @return
+     * @return Value of the first tag of current XML
      */
     public String value() {
         return XmlUtils.getFirstTagValue(xml);
