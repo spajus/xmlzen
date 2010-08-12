07a52811f2c9e0c0437e1c93ca828b65170d6b7a
tomas.varaneckas <tomas.varaneckas@52ce8190-9228-11de-8e1b-e3dee1b55d7a>
2010-08-12 18:18:06 +0000

    Adding comments and cleaning up code.

--- src/main/java/com/googlecode/xmlzen/XmlBuilderOutput.java
+++ src/main/java/com/googlecode/xmlzen/XmlBuilderOutput.java
@@ -1,9 +1,49 @@
+/*
+ * Copyright 2009-2010 Tomas Varaneckas
+ * http://www.varaneckas.com
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.googlecode.xmlzen;
 
+/**
+ * This interface defines the target of XmlBuilder output which is gathered
+ * during the building and then used in some ways (displayed, saved to file,
+ * etc). As far as XmlBuilder is concerned, it needs to know length of current
+ * output (which equals to character count) and a way to append a String or
+ * char.
+ *
+ * Implementations of this interface should not care about thread safety.
+ *
+ * @author Tomas Varaneckas &lt;tomas.varaneckas@gmail.com&gt;
+ * @version $Id$
+ */
 public interface XmlBuilderOutput {
 
+    /**
+     * Gives length of contents in characters
+     *
+     * @return content length
+     */
     public long length();
 
+    /**
+     * Appends an object (String or char) to the XML that is currently being
+     * built. Unexpected data types will be converted to String representation.
+     *
+     * @param data Data to be appended to XML that is currently being built.
+     * @return self (for call chaining)
+     */
     public XmlBuilderOutput append(Object data);
 
 }
--- src/main/java/com/googlecode/xmlzen/XmlBuilderStringOutput.java
+++ src/main/java/com/googlecode/xmlzen/XmlBuilderStringOutput.java
@@ -1,12 +1,37 @@
+/*
+ * Copyright 2009-2010 Tomas Varaneckas
+ * http://www.varaneckas.com
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.googlecode.xmlzen;
 
+/**
+ * XmlBuilderOutput implementation that uses StringBuilder to write XML data.
+ * It keeps the data in memory, so it's not good with big XMLs.
+ *
+ * This implementation is used by default. You can get the results of building
+ * by calling toString().
+ *
+ * @author Tomas Varaneckas &lt;tomas.varaneckas@gmail.com&gt;
+ * @version $Id$
+ */
 public class XmlBuilderStringOutput implements XmlBuilderOutput {
 
-    private final StringBuilder target;
-
-    public XmlBuilderStringOutput() {
-        target = new StringBuilder();
-    }
+    /**
+     * Target StringBuilder that contains the data.
+     */
+    private final StringBuilder target = new StringBuilder();
 
     public XmlBuilderOutput append(final Object data) {
         target.append(data);
@@ -17,4 +42,8 @@ public class XmlBuilderStringOutput implements XmlBuilderOutput {
         return target.length();
     }
 
+    @Override
+    public String toString() {
+        return target.toString();
+    }
 }
